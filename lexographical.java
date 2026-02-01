8b828bbc18294ee403845c390fa4baac9fffa35a
Rudra Pratap Singh <rudrapratap112005@gmail.com>
2026-02-01 22:38:08 +0530

    Add files via upload

---
+++ lexographical.java
@@ -0,0 +1,7 @@
+public class lexographical {
+    public static void main (String[] args){
+      String a = "RUdra";
+      String  b = "Nitin";
+      System.out.println(a.compareTo(b));
+    }
+}
