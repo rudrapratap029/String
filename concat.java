71e946efa14cc938e1f129f2c4573ec26ac7bb6a
Rudra Pratap Singh <rudrapratap112005@gmail.com>
2026-02-02 21:52:21 +0530

    Add files via upload

---
+++ concat.java
@@ -0,0 +1,24 @@
+public class concat {
+    public static void main(String [] args){
+
+        String a = "Rudra";
+        String b = "Nitin";
+        String c = a+b;
+        String d = a+" Pratap Singh_07";
+        String e =  'M'   + a;
+        String f = "Rudra" +10 + 20 ;
+        String g =  +10 + 20 +"Rudra" ;
+        String h =  10  +"Rudra"+ 20  ;
+        String i =  'A' + 5 + a  ; // output 70
+
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g); 
+        System.out.println(h);
+        System.out.println(i); 
+
+    }
+}
