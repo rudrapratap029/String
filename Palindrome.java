65726978886566540f6144af7e1913c76ee43307
Rudra Pratap Singh <rudrapratap112005@gmail.com>
2026-02-24 22:44:21 +0530

    Format Palindrome.java with consistent spacing

--- Palindrome.java
+++ Palindrome.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class Palindrome {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         
 
@@ -28,4 +29,5 @@ public class Palindrome {
     }
 
 }
+
 
