030a7867c0bafe9d6f08197a12597ce4489432a1
Rudra Pratap Singh <rudrapratap112005@gmail.com>
2026-03-04 22:53:50 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Palindrome {
     public static void main(String[] args) {
         
+        
         Scanner sc = new Scanner(System.in);
         
 
@@ -29,5 +30,6 @@ public class Palindrome {
     }
 
 }
+
 
 
