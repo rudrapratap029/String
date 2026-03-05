f4bdcd011810b4f4bba524c4ed3f9801abf95064
Rudra Pratap Singh <rudrapratap112005@gmail.com>
2026-03-05 09:39:59 +0530

    Add files via upload

---
+++ Contains.java
@@ -0,0 +1,13 @@
+public class Contains {
+
+    public static void main (String[] args){
+        String s = "Harshit";
+        System.out.println(s.contains("Harsh"));
+
+        if(s.contains ("sh")){
+              System.out.println("Happy");
+        }
+    }
+
+    
+}
