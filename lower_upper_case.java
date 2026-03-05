c84f4d19c9b6a1d371bb5a7450d4ea14646af427
Rudra Pratap Singh <rudrapratap112005@gmail.com>
2026-03-05 09:39:32 +0530

    Add files via upload

---
+++ lower_upper_case.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class lower_upper_case {
+    public static void main (String[] args){
+     Scanner sc = new Scanner(System.in);
+     String s = sc.next();
+     for(int i = 0 ; i<s.string() ; i++){
+        if(s.charAt(0) &&  )
+     }
+    }
+}
