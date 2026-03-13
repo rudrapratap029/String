be1588c10f51cb4c1ff127973609b3ab59a47a1b
Rudra Pratap Singh <rudrapratap112005@gmail.com>
2026-03-13 21:12:33 +0530

    Fix formatting issues in SubString.java

--- SubString.java
+++ SubString.java
@@ -2,6 +2,7 @@ public class SubString {
 
     public static void main(String[] args) {
         String s = "Rudra";
+        
         for (int i = 0; i < s.length(); i++) {
             for (int j = i+1; j <= s.length(); j++) {
                 System.out.println(s.substring(i, j) + " ");
@@ -12,3 +13,4 @@ public class SubString {
     }
 
 }
+
